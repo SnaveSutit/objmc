14d53f55a4f70e53c7e844242e0308cb286454a7
SnaveSutit <snavesutit@gmail.com>
2023-01-26 21:31:12 -0500

    skibidi

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
+import ObjFile from 'obj-file-parser'
+import * as fs from 'fs/promises'
 
+const DEBUG_PATH = 'D:/github-repos/mcmap-plush-rush/objmodels/cow.obj'
 
+async function main() {
+	const fileContents = await fs.readFile(DEBUG_PATH, 'utf-8')
+	const obj = new ObjFile(fileContents)
+	const content = obj.parse()
+	await fs.writeFile('./output.json', JSON.stringify(content, null, '\t'))
+}
+
+main()
